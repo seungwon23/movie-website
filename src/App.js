518542132e0564c29bd6b35e72290ca3105a5fab
seungwon <seungwonj45@naver.com>
2024-08-01 22:14:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return <BrowserRouter>
     <Routes>
       <Route path="/movie/:id" element={<Detail />} />
-      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+      <Route path="/" element={<Home />} />
     </Routes>
     </BrowserRouter>;
 }
